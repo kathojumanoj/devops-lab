daa19196b71de6ab2a0024c9ecd47734f0937e2f
kathojumanoj <139505502+kathojumanoj@users.noreply.github.com>
2024-11-22 00:47:58 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 class HelloWorld{
   public static void main(String args[])
 {
-  System.out.println("Hello World Java To Manoj");
+  System.out.println("Hello World Java To Manoj king kathoju");
 }
 }
