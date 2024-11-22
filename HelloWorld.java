cb23840c9e40bfe091edda190c2d0113d2eae3e8
kathojumanoj <139505502+kathojumanoj@users.noreply.github.com>
2024-11-22 19:12:33 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,15 +1,15 @@
 class HelloWorld{
   public static void main(String args[])
 {
-  // System.out.println("Hello World Java To Manoj king kathoju");
-  System.out.println(" Devops Labs External");
-  if(args.length>0)
-  {
-      if(args[0]=="1")
-        System.out.println(" Valid User for Login Page :))))");
-      else
-        System.out.println(" Invalid User for Login Page :((");
-  }
+  System.out.println("Hello World Java To Manoj king kathoju");
+  // System.out.println(" Devops Labs External");
+  // if(args.length>0)
+  // {
+  //     if(args[0]=="1")
+  //       System.out.println(" Valid User for Login Page :))))");
+  //     else
+  //       System.out.println(" Invalid User for Login Page :((");
+  // }
           
   
 }
